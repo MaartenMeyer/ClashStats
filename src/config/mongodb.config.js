e440e9b35321651f4f7b06956fbf79d732b49355
Maarten <maartenmeyer8@gmail.com>
2019-11-26 17:26:03 +0100

    mongodb admin update

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -6,6 +6,6 @@ module.exports = {
     url: 'mongodb://localhost:27017/clashstats'
   },
   production: {
-    url: `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@clashstats-main-ep9ez.azure.mongodb.net/clashstats?retryWrites=true&w=majority`
+    url: `mongodb+srv://clashstats-admin:${process.env.MONGODB_PASSWORD}@clashstats-main-ep9ez.azure.mongodb.net/clashstats?retryWrites=true&w=majority`
   }
 }
