45970093754f6bd44d70e37f28e733036c6675a5
Maarten <maartenmeyer8@gmail.com>
2019-11-26 17:01:46 +0100

    Added production connection string

--- src/config/mongodb.config.js
+++ src/config/mongodb.config.js
@@ -4,5 +4,8 @@ module.exports = {
   },
   development: {
     url: 'mongodb://localhost:27017/clashstats'
+  },
+  production: {
+    url: `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@clashstats-main-ep9ez.azure.mongodb.net/clashstats?retryWrites=true&w=majority`
   }
 }
